2edd00053651bf171088655f018e262d1d3a7672
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 15:04:39 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 5;
+			bool result = IsOdd(number);
+			Console.WriteLine(result);
+
+			number = 6;
+			result = IsEven(number);
+			Console.WriteLine(result);
+		}
+		static bool IsOdd(int source)
+		{
+			if (source % 2 != 0)
+				return true;
+			else return false;
+		}
+
+		static bool IsEven(int source)
+		{
+			if (source % 2 == 0)
+				return true;
+			else return false;
+		}
+	}
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string value = "123456";
+			int result = value.ToInt(0);
+			Console.WriteLine(result);
+
+			value = "AAA";
+			result = value.ToInt(0);
+			Console.WriteLine(result);
+		}
+	}
+	public static class Extensions
+	{
+		public static int ToInt(this string value,int defaultValue)
+		{
+			int result = 0;
+			bool canConvert2Int = int.TryParse(value,out defaultValue);
+			if (canConvert2Int==true) return result;
+			else return defaultValue;
+		}
+	}
+}
